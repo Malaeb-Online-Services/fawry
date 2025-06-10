ddc0b4e0f77384739be054cd33b0f903a36ef6df
AymanElshehawy <ayman@malaebapp.com>
2025-06-10 15:35:21 +0300

    fix: standardize Lang facade usage and translation keys

--- src/Validation/PaymentValidation.php
+++ src/Validation/PaymentValidation.php
@@ -21,7 +21,7 @@ class PaymentValidation
         
         foreach ($requiredFields as $field) {
             if (!isset($params[$field])) {
-                throw new PaymentException(message: Lang::get('fawry::exceptions.payment.required_field_missing', ['field' => $field]));
+                throw new PaymentException(message: Lang.get('fawry.exceptions.payment.required_field_missing', ['field' => $field]));
             }
         }
     }
@@ -32,7 +32,7 @@ class PaymentValidation
         
         foreach ($requiredFields as $field) {
             if (!isset($userData[$field]) || empty($userData[$field])) {
-                throw new PaymentException(Lang::get('fawry::exceptions.payment.required_user_field_missing', ['field' => $field]));
+                throw new PaymentException(Lang.get('fawry.exceptions.payment.required_user_field_missing', ['field' => $field]));
             }
         }
     }
@@ -40,16 +40,16 @@ class PaymentValidation
     protected static function validateItems(array $items): void
     {
         if (empty($items)) {
-            throw new PaymentException(Lang::get('fawry::exceptions.payment.items.required'));
+            throw new PaymentException(Lang::get('fawry.exceptions.payment.items.required'));
         }
 
         foreach ($items as $index => $item) {
             if (!isset($item['id'], $item['price']) || empty($item['id']) || !is_numeric($item['price'])) {
-                throw new PaymentException(Lang::get('fawry::exceptions.payment.items.invalid'));
+                throw new PaymentException(Lang::get('fawry.exceptions.payment.items.invalid'));
             }
 
             if (isset($item['quantity']) && (!is_numeric($item['quantity']) || $item['quantity'] < 1)) {
-                throw new PaymentException(Lang::get('fawry::exceptions.payment.items.invalid_quantity'));
+                throw new PaymentException(Lang::get('fawry.exceptions.payment.items.invalid_quantity'));
             }
         }
     }
@@ -57,11 +57,11 @@ class PaymentValidation
     protected static function validateUrls(array $params): void
     {
         if (!filter_var($params['redirect_url'], FILTER_VALIDATE_URL)) {
-            throw new PaymentException(Lang::get('fawry::exceptions.payment.invalid_redirect_url'));
+            throw new PaymentException(Lang::get('fawry.exceptions.payment.invalid_redirect_url'));
         }
 
         if (isset($params['webhook_url']) && !filter_var($params['webhook_url'], FILTER_VALIDATE_URL)) {
-            throw new PaymentException(Lang::get('fawry::exceptions.payment.invalid_webhook_url'));
+            throw new PaymentException(Lang::get('fawry.exceptions.payment.invalid_webhook_url'));
         }
     }
 } 
